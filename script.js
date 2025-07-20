986a1cfaff072612bfa1615f5e633c3cd375c46e
unknown <129291511+dedsocks@users.noreply.github.com>
2025-07-20 11:32:29 +0530

    Create a function to return human choice

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log(getComputerChoice());
+console.log(getHumanChoice());
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3)
@@ -13,3 +14,11 @@ function getComputerChoice(){
     }
 }
 
+function getHumanChoice(){
+    let humanChoice = prompt("Enter one of the three choices given below:"+
+    "\n1 . Rock (Beats Scissors)"+
+    "\n2 . Paper (Beats Rock)"+
+    "\n3 . Scissor (Beats Paper)\n");
+    
+    return humanChoice.toLowerCase();
+}
